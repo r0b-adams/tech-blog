a56055b82a3755b3481fa528bc291e891d091b85
Robert Adams <adamsiii.robert@gmail.com>
2022-09-18 14:09:14 -0700

    Import routes and pass to Express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import controllers from './controllers';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(controllers);
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
