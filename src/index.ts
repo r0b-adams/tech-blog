163612b6cad2ce053ec4623cb526b62b575d69cf
Robert Adams <adamsiii.robert@gmail.com>
2023-01-02 16:09:20 -0800

    use yarn

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import controllers from './controllers';
+import express from "express";
+import controllers from "./controllers";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -8,4 +8,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(controllers);
 
-app.listen(PORT, () => console.log(`server listening on ${PORT}`));
+app.listen(PORT, () => console.log(`server listening on PORT ${PORT}`));
