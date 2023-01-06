9ac9941a1f258aced5083ec9910611745ea38bbc
Robert Adams <adamsiii.robert@gmail.com>
2023-01-06 12:39:35 -0800

    add basic User sequelize model

---
+++ src/models/User.ts
@@ -0,0 +1,31 @@
+import { db } from "../services";
+import { DataTypes } from "sequelize";
+
+export const User = db.connection.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: true,
+    underscored: true,
+  }
+);
