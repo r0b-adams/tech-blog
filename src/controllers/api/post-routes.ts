695ff0a63443de20a6bcc46c6df4ec004245b0c2
Robert Adams <adamsiii.robert@gmail.com>
2023-01-06 11:34:32 -0800

    rename folder

--- src/controllers/api/post-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('*', (req, res) => {
-  res.status(200).json({ message: 'POST ROUTES HERE' });
-});
-
-export default router;
