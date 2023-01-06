26d4a2b437890b832b1c615510bda3022a28564a
Robert Adams <adamsiii.robert@gmail.com>
2023-01-06 11:36:22 -0800

    add config file to ref env vars

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const SERVER_PORT = process.env.PORT || 3001;
+export const DB_CONFIG = {
+  URI: process.env.DB_URI || "",
+  DATABASE: process.env.DB_NAME || "",
+  USERNAME: process.env.DB_USERNAME || "",
+  PASSWORD: process.env.DB_PASSWORD || "",
+  SESSION_SECRET: process.env.SESSION_SECRET || "",
+};
