6f0977cbc9eaaefe23df4c783e8b096beeab5929
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 10:58:49 -0800

    add login route

--- src/controllers/api/users/index.ts
+++ src/controllers/api/users/index.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import * as users from "./handlers";
+import * as users from "./UserHandlers";
 
 // /api/users
-
 const router = Router();
 
 router.post("/signup", users.signup);
+router.post("/login", users.login);
 
 export { router as userRoutes };
