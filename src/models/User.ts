26c14d0d3e5c27d11e9c05f8828b77a7523de238
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 11:00:25 -0800

    update id column, add check pw instance method

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,40 +6,59 @@ import {
   InferCreationAttributes,
   CreationOptional,
 } from "sequelize";
+import bcrypt from "bcrypt";
 
-interface UserModel
-  extends Model<InferAttributes<UserModel>, InferCreationAttributes<UserModel>> {
-  id: CreationOptional<number>;
-  username: string;
-  email: string;
-  password: string;
+export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  // table colums
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare email: string;
+  declare password: string;
+  declare created_at: CreationOptional<Date>;
+  declare updated_at: CreationOptional<Date>;
+  declare deleted_at: CreationOptional<Date>;
+
+  // model instance methods
+  async checkPassword(password: string) {
+    return await bcrypt.compare(password, this.password);
+  }
 }
 
-export const User = db.connection.define<UserModel>(
-  "User",
+User.init(
   {
     id: {
-      type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true,
+      type: DataTypes.UUIDV4,
+      defaultValue: DataTypes.UUIDV4,
       allowNull: false,
     },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isAlphanumeric: true,
+      },
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE,
+    deleted_at: DataTypes.DATE,
   },
   {
+    sequelize: db.connection,
     timestamps: true,
-    // underscored: true seems to be buggy and returns camelCase on create so format manually
     createdAt: "created_at",
     updatedAt: "updated_at",
     deletedAt: "deleted_at",
