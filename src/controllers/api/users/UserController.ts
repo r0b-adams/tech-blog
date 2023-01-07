d4d10092ac6b06f6566ac2655a4da98e4d73f4d1
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 10:59:14 -0800

    add login and find user methods

--- src/controllers/api/users/UserController.ts
+++ src/controllers/api/users/UserController.ts
@@ -3,7 +3,27 @@ import { User } from "../../../models";
 export class UserController {
   private static User = User;
 
-  public static signup(username: string, email: string, password: string) {
-    return this.User.create({ username, email, password });
+  public static async adminFindUserByUsername(username: string) {
+    return await this.User.findOne({ where: { username } });
+  }
+
+  public static async signup(username: string, email: string, password: string) {
+    const user = await this.adminFindUserByUsername(username);
+    if (user) {
+      throw new Error("an account with that username exists");
+    }
+    return await this.User.create({ username, email, password });
+  }
+
+  public static async login(username: string, password: string) {
+    const user = await this.adminFindUserByUsername(username);
+    if (!user) {
+      throw new Error("wrong username or password");
+    }
+    const validPassword = await user.checkPassword(password);
+    if (!validPassword) {
+      throw new Error("wrong username or password");
+    }
+    return user;
   }
 }
