028307ebf63fac1518a8cb4d709ecc84bd095fb7
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 23:11:28 -0800

    setup handlebars as view engine

---
+++ src/views/index.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import { Express } from "express";
+import { create } from "express-handlebars";
+import * as helpers from "./helpers";
+
+export const hbs = create({ helpers });
+
+export const setViewEngine = (app: Express) => {
+  app.engine("handlebars", hbs.engine);
+  app.set("view engine", "handlebars");
+  app.set("views", path.join(__dirname, "/views"));
+};
