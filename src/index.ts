dfa7d8e16ef44d469c49379ca8eef2b1cd007a6d
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 17:05:51 -0800

    apply err req handler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import { SERVER_PORT } from "./config";
 import { routes } from "./controllers";
 import { db, sessions } from "./services";
-import { SERVER_PORT } from "./config";
+import { errorhandlers } from "./middleware";
 
 const app = express();
 
@@ -9,9 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(sessions);
 app.use(routes);
+app.use(errorhandlers.catchAll);
 
 app.listen(SERVER_PORT, async () => {
   const connection = await db.open();
+  await connection.sync({ force: true });
   console.log(`connected to database ${connection.config.database}`);
   console.log(`listening on port ${SERVER_PORT}`);
 });
