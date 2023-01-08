c8c7469c158247268655a1aa701109e539db3b5d
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 23:10:44 -0800

    factor out view engine setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import { SERVER_PORT } from "./config";
-import { routes } from "./controllers";
 import { db, sessions } from "./services";
-import { errorhandlers } from "./middleware";
+import { setViewEngine } from "./views";
+import { routes } from "./controllers";
+import { errorHandlers } from "./middleware";
 
 const app = express();
 
-app.use(express.json());
+setViewEngine(app);
+
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(sessions);
 app.use(routes);
-app.use(errorhandlers.catchAll);
+app.use(errorHandlers.catchAll);
 
 app.listen(SERVER_PORT, async () => {
   const connection = await db.open();
