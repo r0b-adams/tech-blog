928f88042b2b438bd806e4d275b891477874c21a
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 20:06:33 -0800

    apply comment routes to app

--- src/controllers/api/index.ts
+++ src/controllers/api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userRoutes } from "./users";
 import { postRoutes } from "./posts";
+import { commentRoutes } from "./comments";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 export { router as apiRoutes };
