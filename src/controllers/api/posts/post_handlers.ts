fa283a936674a841e2e5e847648463b5ed43391a
Robert Adams <adamsiii.robert@gmail.com>
2023-01-07 20:08:12 -0800

    update res status codes

--- src/controllers/api/posts/post_handlers.ts
+++ src/controllers/api/posts/post_handlers.ts
@@ -6,7 +6,7 @@ export const create: RequestHandler = async (req, res, next) => {
     const { user_id } = req.session;
     const { title, content } = req.body;
     const post = await PostController.createPost(user_id!, title, content);
-    res.status(200).json({ msg: "post created", post });
+    res.status(201).json({ msg: "post created", post });
   } catch (error) {
     next(error);
   }
@@ -14,8 +14,8 @@ export const create: RequestHandler = async (req, res, next) => {
 
 export const update: RequestHandler = async (req, res, next) => {
   try {
-    const { user_id } = req.session;
     const { post_id } = req.params;
+    const { user_id } = req.session;
     const { title, content } = req.body;
     await PostController.updatePost(post_id, user_id!, title, content);
     res.status(200).json({ msg: "post updated" });
@@ -26,10 +26,10 @@ export const update: RequestHandler = async (req, res, next) => {
 
 export const destroy: RequestHandler = async (req, res, next) => {
   try {
-    const { user_id } = req.session;
     const { post_id } = req.params;
+    const { user_id } = req.session;
     await PostController.deletePost(post_id, user_id!);
-    res.status(200).json({ msg: "post deleted" });
+    res.status(204).json({ msg: "post deleted" });
   } catch (error) {
     next(error);
   }
