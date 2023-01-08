04f46798e460104eb0cf22cb09fc2aaeadcd48ea
Robert Adams <adamsiii.robert@gmail.com>
2023-01-08 10:20:36 -0800

    return posts with formatting

--- src/controllers/api/posts/PostController.ts
+++ src/controllers/api/posts/PostController.ts
@@ -3,7 +3,8 @@ import { Post, User } from "../../../models";
 
 export class PostController {
   static async createPost(user_id: string, title: string, content: string) {
-    return await Post.create({ user_id, title, content });
+    const post = await Post.create({ user_id, title, content });
+    return post.get({ plain: true });
   }
 
   static async updatePost(id: string, user_id: string, title: string, content: string) {
@@ -20,18 +21,20 @@ export class PostController {
   }
 
   static async getAllPosts() {
-    return await Post.findAll({
+    const posts = await Post.findAll({
       order: [["created_at", "DESC"]],
       attributes: ["id", "title", "content", "created_at"],
       include: { model: User, attributes: ["username"] },
     });
+    return posts.map(model => model.get({ plain: true }));
   }
 
   static async getUserPosts(user_id: string) {
-    return await Post.findAll({
+    const posts = await Post.findAll({
       where: { user_id },
       order: [["created_at", "DESC"]],
       attributes: ["id", "title", "content", "created_at"],
     });
+    return posts.map(model => model.get({ plain: true }));
   }
 }
