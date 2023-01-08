183aef0b3047e0446e0cb26d8a1a41add8695bd2
Robert Adams <adamsiii.robert@gmail.com>
2023-01-08 10:20:18 -0800

    add express static middleware

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import { SERVER_PORT } from "./config";
 import { db, sessions } from "./services";
@@ -9,8 +10,12 @@ const app = express();
 
 setViewEngine(app);
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const static_url = path.join(__filename, "../public");
+console.log(static_url);
+app.use(express.static(static_url));
 app.use(sessions);
 app.use(routes);
 app.use(errorHandlers.catchAll);
