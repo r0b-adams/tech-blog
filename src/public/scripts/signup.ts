3045e78445a610721581ed33288716ac993f3b3d
Robert Adams <adamsiii.robert@gmail.com>
2023-01-08 23:17:20 -0800

    add signup script

---
+++ src/public/scripts/signup.ts
@@ -0,0 +1,25 @@
+const signup_form = document.getElementById("signup");
+
+signup_form?.addEventListener("submit", async e => {
+  const [un_input, em_input, pw_input] = signup_form.querySelectorAll("input");
+  try {
+    e.preventDefault();
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: un_input.value,
+        password: pw_input.value,
+      }),
+    });
+
+    if (response.ok) {
+      window.location.replace("/dashboard");
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
