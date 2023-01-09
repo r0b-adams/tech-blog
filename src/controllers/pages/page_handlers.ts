2fabc38ece582d0d154009f42e539c69670708f9
Robert Adams <adamsiii.robert@gmail.com>
2023-01-09 12:05:58 -0800

    add signlePost view, update page render config

--- src/controllers/pages/page_handlers.ts
+++ src/controllers/pages/page_handlers.ts
@@ -1,13 +1,15 @@
 import { RequestHandler } from "express";
 import { PostController } from "../api/posts/PostController";
 
+// /
 export const home: RequestHandler = async (req, res, next) => {
   try {
+    const { user_id } = req.session;
     const posts = await PostController.getAllPosts();
     res.render("home", {
       layout: "main",
-      home: true,
-      logged_in: req.session.user_id ? true : false,
+      page: { home: true },
+      logged_in: user_id ? true : false,
       posts,
     });
   } catch (error) {
@@ -15,14 +17,15 @@ export const home: RequestHandler = async (req, res, next) => {
   }
 };
 
+// /dashboard
 export const dashboard: RequestHandler = async (req, res, next) => {
   try {
     const { user_id } = req.session;
     const posts = await PostController.getUserPosts(user_id!);
     res.render("dashboard", {
       layout: "main",
-      dashboard: true,
-      logged_in: req.session.user_id ? true : false,
+      page: { dashboard: true },
+      logged_in: user_id ? true : false,
       posts,
     });
   } catch (error) {
@@ -30,35 +33,47 @@ export const dashboard: RequestHandler = async (req, res, next) => {
   }
 };
 
+// signup
 export const signup: RequestHandler = async (req, res, next) => {
   try {
+    const { user_id } = req.session;
     res.render("signup", {
       layout: "main",
-      signup: true,
-      logged_in: req.session.user_id ? true : false,
+      page: { signup: true },
+      logged_in: user_id ? true : false,
     });
   } catch (error) {
     next(error);
   }
 };
 
+// /login
 export const login: RequestHandler = async (req, res, next) => {
   try {
+    const { user_id } = req.session;
     res.render("login", {
       layout: "main",
-      login: true,
-      logged_in: req.session.user_id ? true : false,
+      page: { login: true },
+      logged_in: user_id ? true : false,
     });
   } catch (error) {
     next(error);
   }
 };
 
+// /posts/:post_id
 export const singlePost: RequestHandler = async (req, res, next) => {
   try {
     const { post_id } = req.params;
     const { user_id } = req.session;
-    res.status(200).json({ message: "SINGLE POST VIEW" });
+    const post = await PostController.getPost(post_id);
+    res.render("single-post", {
+      layout: "main",
+      page: { single_post: true },
+      post,
+      editable: user_id === post?.user_id,
+      logged_in: user_id ? true : false,
+    });
   } catch (error) {
     next(error);
   }
