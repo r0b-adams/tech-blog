8cb9c7f96f1d78a3c323b231c1a8d1124b0fe5e3
Robert Adams <adamsiii.robert@gmail.com>
2023-01-10 11:48:44 -0800

    rename file

--- src/controllers/pages/viewHandlers.ts
+++ src/controllers/pages/viewHandlers.ts
@@ -24,7 +24,7 @@ export const dashboard: RequestHandler = async (req, res, next) => {
     const posts = await PostController.getUserPosts(user_id!);
     res.render("dashboard", {
       layout: "main",
-      page: { dashboard: true },
+      view: { dashboard: true },
       logged_in: user_id ? true : false,
       posts,
     });
@@ -33,13 +33,13 @@ export const dashboard: RequestHandler = async (req, res, next) => {
   }
 };
 
-// signup
+// /signup
 export const signup: RequestHandler = async (req, res, next) => {
   try {
     const { user_id } = req.session;
     res.render("signup", {
       layout: "main",
-      page: { signup: true },
+      view: { signup: true },
       logged_in: user_id ? true : false,
     });
   } catch (error) {
@@ -53,7 +53,7 @@ export const login: RequestHandler = async (req, res, next) => {
     const { user_id } = req.session;
     res.render("login", {
       layout: "main",
-      page: { login: true },
+      view: { login: true },
       logged_in: user_id ? true : false,
     });
   } catch (error) {
@@ -67,11 +67,27 @@ export const singlePost: RequestHandler = async (req, res, next) => {
     const { post_id } = req.params;
     const { user_id } = req.session;
     const post = await PostController.getPost(post_id);
-    res.render("single-post", {
+    res.render("post", {
       layout: "main",
-      page: { single_post: true },
-      post,
-      editable: user_id === post?.user_id,
+      view: { single_post: true },
+      logged_in: user_id ? true : false,
+      post: {
+        ...post,
+        editable: user_id === post?.user_id,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// /dashboard/new-post
+export const newPost: RequestHandler = async (req, res, next) => {
+  try {
+    const { user_id } = req.session;
+    res.render("new-post", {
+      layout: "main",
+      view: { new_post: true },
       logged_in: user_id ? true : false,
     });
   } catch (error) {
@@ -79,7 +95,7 @@ export const singlePost: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const code404: RequestHandler = async (req, res, next) => {
+export const _404: RequestHandler = async (req, res, next) => {
   try {
     res.status(200).json({ message: "404 NOT FOUND VIEW" });
   } catch (error) {
