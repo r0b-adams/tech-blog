17c1769c1f4509e360eb2c846966d90740d49668
Robert Adams <adamsiii.robert@gmail.com>
2023-01-10 21:53:24 -0800

    add edit route

--- src/controllers/views/index.ts
+++ src/controllers/views/index.ts
@@ -5,10 +5,11 @@ import * as views from "./viewHandlers";
 const router = Router();
 
 router.get("/", views.home);
-router.get("/dashboard", authorize, views.dashboard);
-router.get("/dashboard/new-post", authorize, views.newPost);
 router.get("/signup", views.signup);
 router.get("/login", views.login);
+router.get("/dashboard", authorize, views.dashboard);
+router.get("/dashboard/new", authorize, views.newPost);
+router.get("/dashboard/edit/:post_id", authorize, views.editPost);
 router.get("/posts/:post_id", views.singlePost);
 router.get("*", views._404);
 
