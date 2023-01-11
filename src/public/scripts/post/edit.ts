b8383c1d6c6fe8fe0792b3fa52e4a07a5899bce2
Robert Adams <adamsiii.robert@gmail.com>
2023-01-10 23:12:14 -0800

    add edit and delete functionality

--- src/public/scripts/post/edit.ts
+++ src/public/scripts/post/edit.ts
@@ -1,1 +1,45 @@
-console.log("edit linked");
+const deleteBtn = document?.querySelector("button#delete") as HTMLButtonElement;
+const editPostForm = document?.getElementById("edit-post-form") as HTMLFormElement;
+deleteBtn?.addEventListener("click", async e => {
+  try {
+    const post_id = editPostForm.dataset.post_id;
+    const response = await fetch(`/api/posts/${post_id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      window.location.replace("/dashboard");
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+editPostForm?.addEventListener("submit", async e => {
+  try {
+    e.preventDefault();
+    const post_id = editPostForm.dataset.post_id;
+    const [titleInput, contentTextarea] = Array.from(editPostForm.elements);
+    const title = (titleInput as HTMLInputElement).value;
+    const content = (contentTextarea as HTMLTextAreaElement).value;
+    if (title && content) {
+      const response = await fetch(`/api/posts/${post_id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title, content }),
+      });
+      if (response.ok) {
+        window.location.replace(`/posts/${post_id}`);
+      } else {
+        const data = await response.json();
+        console.log(data);
+      }
+    } else {
+      alert("need title and content");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
