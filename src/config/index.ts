942b189c192ca87f238bf8d8c5dd49bc0cb2c4a0
Robert Adams <adamsiii.robert@gmail.com>
2023-01-10 18:01:58 -0800

    export enum

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,15 @@
 import * as dotenv from "dotenv";
+export { ENV_TYPE } from "./types";
+
 dotenv.config();
 
+export const NODE_ENV = process.env.NODE_ENV || "DEVELOPMENT";
 export const SERVER_PORT = process.env.PORT || 3001;
 export const DB_CONFIG = {
   URI: process.env.DB_URI || "",
   DATABASE: process.env.DB_NAME || "",
   USERNAME: process.env.DB_USERNAME || "",
   PASSWORD: process.env.DB_PASSWORD || "",
-  HASH_SALT_ROUNDS: 10,
+  PASSWORD_HASH_SALT_ROUNDS: 10,
   SESSION_SECRET: process.env.SESSION_SECRET || "",
 };
