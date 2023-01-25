9ff80b8e7e7fb41e5dc40d3da41142b9d4796275
Robert Adams <adamsiii.robert@gmail.com>
2023-01-25 10:15:54 -0800

    Add get_year view helper

--- src/utils/viewHelpers.ts
+++ src/utils/viewHelpers.ts
@@ -2,3 +2,8 @@ export const format_date = (ISO: string) => {
   const date = new Date(ISO);
   return date.toLocaleString();
 };
+
+export const get_year = () => {
+  const date = new Date();
+  return date.getFullYear();
+};
